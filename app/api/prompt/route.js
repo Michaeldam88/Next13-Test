e21fff2ae943aa32dfb1d5f1de8abd1e09dcc641
Michaeldam88 <michaeldam88@gmail.com>
2023-07-12 20:14:42 +0200

    hotfix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,11 +5,15 @@ export const GET = async (request) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
-    const response = new Response(JSON.stringify(prompts), { status: 200 });
-    response.headers.set(
-      "Cache-Control",
-      "no-cache, no-store, max-age=0, must-revalidate"
-    );
+    const response = new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        "Cache-Control": "public, s-maxage=1",
+        "CDN-Cache-Control": "public, s-maxage=60",
+        "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
+      },
+    });
+
     return response;
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
